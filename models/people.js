45362e92a74505ea8b58545bfe0b00d276cc918f
Isaiah Romo <iromo237@west-mec.org>
2021-02-22 17:46:46 -0700

    People Controller and model
    Went through and set up the controller and model still fixing it but got a base down.

---
+++ models/people.js
@@ -0,0 +1,74 @@
+var mongoose = require('mongoose');
+const { DateTime } = require("luxon");
+
+var Schema = mongoose.Schema;
+
+var PeopleSchema = new Schema(
+    {
+        p_code: { required: true },
+        first_name: { type: String, required: true, maxlength: 100 },
+        middle_intial: { type: String, required: true, maxlength: 1 },
+        last_name: { type: String, required: true, maxlength: 100 },
+        date_of_birth: { type: Date, required: true },
+        date_of_death: { type: Date },
+        origin: { required: true },
+        hair_color: { required: true },
+        race: { required: true },
+        ssn_dl: { type: Number, maxlength: 9 },
+        smt: { type: String, required: true },
+        address: { type: address, required: true },
+        phone_number: { type: phone_number, required: true },
+        height: { type: Number, required: true, maxlength: 3 },
+        gang_aff: {  },
+        hazard: {  },
+        weight: { type: Number, required: true, maxlength: 3 },
+        eye_color: { type: String, required: true, maxlength: 3 },
+    }
+);
+
+// Virtual for author's full name
+PeopleSchema.virtual('name').get(function () {
+    return this.first_name + ' ' + this.middle_intial + ' ' + this.last_name;
+});
+
+// Virtual for person's date of birth
+PeopleSchema.virtual('lifespan').get(function () {
+    var string = '';
+    if (this.date_of_birth) {
+        string = DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED);
+    }
+    string += " - ";
+    var string = '';
+    if (this.date_of_death) {
+        string = DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED);
+    }
+    return string;
+});
+
+// Virtual for person's hair color
+PeopleSchema.virtual('name').get(function () {
+    return this.hair_color + ' ' + this.eye_color;
+});
+
+// Virtual for person's weight and height
+PeopleSchema.virtual('name').get(function () {
+    return this.weight + ' ' + this.height;
+});
+
+// Virtual for person's origin
+PeopleSchema.virtual('name').get(function () {
+    return this.origin;
+});
+
+// Virtual for author's URL
+PeopleSchema.virtual('url').get(function () {
+    return '/catalog/author/' + this._id;
+});
+
+
+AuthorSchema.virtual('date_of_birth_yyyy_mm_dd').get(function () {
+    return DateTime.fromJSDate(this.date_of_birth).toISODate(); //format 'YYYY-MM-DD'
+});
+
+//Export model
+module.exports = mongoose.model('Author', AuthorSchema);
