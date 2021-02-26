9cd1294b1ec2080eac783a952c47cdb087ec7285
Isaiah Romo <iromo237@west-mec.org>
2021-02-26 16:58:54 -0700

    Routes set up
    got some routes linked but not all of them

--- models/people.js
+++ models/people.js
@@ -5,7 +5,7 @@ var Schema = mongoose.Schema;
 
 var PeopleSchema = new Schema(
     {
-        p_code: { type: String, required: true, maxlength: 100 },
+        // p_code: { type: String, required: true, maxlength: 100 },
         first_name: { type: String, required: true, maxlength: 100 },
         middle_intial: { type: String, required: true, maxlength: 1 },
         last_name: { type: String, required: true, maxlength: 100 },
@@ -13,14 +13,14 @@ var PeopleSchema = new Schema(
         date_of_death: { type: Date, required: false },
         origin: { type: String, required: true, maxlength: 100 },
         hair_color: { type: String, required: true, maxlength: 100 },
-        race: { type: String, required: true, maxlength: 100 },
-        ssn_dl: { type: Number, maxlength: 9 },
-        smt: { type: String, required: true },
-        address: { type: String, required: true, maxlength: 100 },
-        phone_number: { type: String, required: true, maxlength: 100 },
+        // race: { type: String, required: true, maxlength: 100 },
+        // ssn_dl: { type: Number, maxlength: 9 },
+        // smt: { type: String, required: true },
+        // address: { type: String, required: true, maxlength: 100 },
+        // phone_number: { type: String, required: true, maxlength: 100 },
         height: { type: Number, required: true, maxlength: 3 },
-        gang_aff: { type: String, maxlength: 100, required: false },
-        hazard: { type: String, maxlength: 100, required: false },
+        // gang_aff: { type: String, maxlength: 100, required: false },
+        // hazard: { type: String, maxlength: 100, required: false },
         weight: { type: Number, required: true, maxlength: 3 },
         eye_color: { type: String, required: true, maxlength: 3 },
     }
